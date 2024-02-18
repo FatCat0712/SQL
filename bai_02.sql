42c4096efbf471c9785a7ebc841f61b0595d0719
FatCat0712 <maisonhai456@gmail.com>
2024-02-18 21:16:14 +0700

    Getting started with SQL

---
+++ bai_02.sql
@@ -0,0 +1,6 @@
+-- tạo CSDL
+CREATE DATABASE vidu CHARSET "utf8mb4" COLLATE "utf8mb4_general_ci";
+-- chỉnh sửa
+ALTER DATABASE vidu CHARSET "utf8mb4" COLLATE "utf8mb4_general_ci";
+-- xóa CSDL
+DROP DATABASE vidu;
