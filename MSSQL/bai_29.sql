c35b2d98786163a8500347bcbc4608723c055afc
FatCat0712 <maisonhai456@gmail.com>
2024-03-04 16:03:03 +0700

    add bai 30

---
+++ MSSQL/bai_29.sql
@@ -0,0 +1,65 @@
+﻿-- liệt kê các đơn hàng có ngày đặt hàng gần đây nhất
+SELECT o.*
+FROM Orders AS o
+WHERE OrderDate = (
+			SELECT MAX(OrderDate)
+			FROM Orders);
+
+-- liệt kê những sản phẩm không có đơn đặt hàng
+SELECT *
+FROM 
+	Products AS p
+WHERE p.ProductID NOT IN ( SELECT DISTINCT ProductID
+							FROM [Order Details]);
+
+-- lấy thông tin về các đơn hàng, và tên các sản phẩm thuộc các đơn hàng
+-- chưa được giao
+SELECT 
+	o.OrderID,
+	p.ProductName 
+FROM Orders AS o
+INNER JOIN
+	[Order Details] AS od
+	ON od.OrderID = o.OrderID
+INNER JOIN 
+	Products AS p
+	ON od.ProductID = p.ProductID
+WHERE o.OrderID IN (
+	SELECT OrderID
+	FROM Orders
+	WHERE ShippedDate IS NULL
+	);
+
+-- lấy thông tin về các sản phẩm có số lượng tồn kho 
+-- ít hơn số lượng tồn kho trung bình của tất cả sản phẩm
+SELECT * 
+FROM Products
+WHERE (UnitsInStock - UnitsOnOrder) < (SELECT AVG(UnitsInStock - UnitsOnOrder)
+						FROM Products);
+
+-- lấy thông tin về khách hàng có tổng giá trị đơn hàng là lớn nhất
+SELECT DISTINCT c.*
+FROM Customers AS c
+INNER JOIN 
+		Orders AS o
+		ON c.CustomerID = o.CustomerID
+WHERE c.CustomerID = (SELECT o.CustomerID
+				 FROM dbo.Orders AS o
+				 WHERE o.OrderID IN
+								(SELECT TOP 1 OrderID
+								FROM [Order Details] AS od
+								GROUP BY OrderID
+								ORDER BY SUM(UnitPrice * Quantity * Discount) DESC));
+
+-- lấy thông tin các sản phẩm có doanh số bán hàng cao nhất
+SELECT *
+FROM Products
+WHERE ProductID = (
+SELECT TOP 1 ProductID
+FROM [Order Details]
+GROUP BY ProductID
+ORDER BY SUM(Quantity * UnitPrice) DESC);
+
+
+
+																
---
+++ MSSQL/bai_30/bai_30.sql
@@ -0,0 +1,52 @@
+﻿SELECT ProductID,ProductName --2
+FROM Products; --1
+
+SELECT ProductID,ProductName --3
+FROM Products --1
+WHERE CategoryID = 1; --2
+
+SELECT CategoryID,COUNT(ProductID) AS "TotalProducts" --4 
+FROM Products --1
+WHERE CategoryID IN (1,2,3) --2
+GROUP BY CategoryID; --3
+
+SELECT CategoryID,COUNT(ProductID) AS "TotalProducts" --5
+FROM Products --1
+WHERE CategoryID IN (1,2,3) --2
+GROUP BY CategoryID --3
+-- HAVING "TotalProducts" >= 13; --4
+-- bị lỗi do cột "TotalProducts" chưa tồn tại
+
+-- fix
+SELECT CategoryID,COUNT(ProductID) AS "TotalProducts" --5
+FROM Products --1
+WHERE CategoryID IN (1,2,3) --2
+GROUP BY CategoryID --3 
+HAVING COUNT(ProductID) >= 13 --4; -- cột "TotalProducts" chưa tồn tại
+
+SELECT CategoryID,COUNT(ProductID) AS "TotalProducts" --5
+FROM Products --1
+WHERE CategoryID IN (1,2,3,4,5,6,7) --2
+GROUP BY CategoryID --3
+HAVING COUNT(ProductID) >= 10 --4
+ORDER BY "TotalProducts"; --6     --cột TotalProducts đã tồn tại
+
+--Exercise
+SELECT 
+TOP 3 --9
+*  --7
+FROM Products AS p --1
+INNER JOIN [Order Details] AS od --2
+ON p.ProductID = od.ProductID --3
+INNER JOIN Orders AS o --4
+ON o.OrderID = od.OrderID --5
+WHERE od.Discount = 0.25 AND o.ShipCountry = 'Germany' --6
+ORDER BY p.ProductID DESC --8;
+
+
+
+
+
+
+
+
