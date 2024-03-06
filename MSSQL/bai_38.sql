61779f48c056b89b0a149f1584c60428d036b16d
FatCat0712 <maisonhai456@gmail.com>
2024-03-06 12:40:29 +0700

    update them bai 39

---
+++ MSSQL/bai_38.sql
@@ -0,0 +1,88 @@
+﻿-- INSERT INTO trong SQL
+-- Cú pháp
+-- thêm dữ liệu cho những cột cụ thể
+-- INSERT INTO table_name (column1, column2, column3, ...)
+-- VALUES (value1, value2, value3, ... );
+
+-- thêm dữ liệu cho toàn bộ cột trong bảng
+-- INSERT INTO table_name
+-- VALUES (value1, value2, value3, ... );
+
+-- thêm một khách hàng mới
+INSERT INTO Customers(CustomerID,CompanyName,ContactName,Phone)
+VALUES ('KH123','TITV.VN','Le Nhat Tung','0123456789');
+
+-- thêm một khách hàng mới đầy đủ các cột
+INSERT INTO Customers
+VALUES
+	('KH456','Bosch','Mai Son Hai','0123456789','-',null,null,null,null,null,null)
+	;
+
+-- thêm một khách hàng mới cùng lúc
+INSERT INTO Customers
+VALUES
+	('KH124','Bosch','Mai Son Hai','0123456789','-',null,null,null,null,null,null),
+	('KH125','FPT','Mai Son Hai','0123456789','-',null,null,null,null,null,null),
+	('KH126','Viettel','Mai Son Hai','0123456789','-',null,null,null,null,null,null),
+	('KH127','Shinhan Finance','Mai Son Hai','0123456789','-',null,null,null,null,null,null)
+	;
+SELECT * FROM Customers
+
+-- nên insert into dữ liệu vào database 1 lần với nhiều dữ liệu
+-- vì mỗi lần gọi lệnh insert dữ liệu sẽ phải ghi vào ổ cứng nên rất mất thời gian, 
+-- tài nguyên
+
+-- Thêm một sản phảm mới vào bảng Products
+INSERT INTO Products(ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock)
+VALUES('Aquafina',1,2,'24 bottles',10.99,100);
+
+SELECT * FROM Products;
+
+-- thêm một đơn hàng mới vào bảng Orders
+INSERT INTO Orders(CustomerID,EmployeeID,OrderDate,ShipVia)
+VALUES ('ALFKI',3,'2023-10-25',2);
+
+-- chèn nhiều khách hàng vào bảng Customers bằng cách sử dụng câu lệnh INSERT INTO
+-- và giá trị VALUES
+INSERT INTO Customers(CustomerID,ContactName,CompanyName,City,Country)
+VALUES
+	('CUST1','Alice Johnson','Johnson Inc','New York','USA'),
+	('CUST2','Bob Smith','Smith Corp','Los Angeles','USA'),
+	('CUST3','Charie Brown','Brown Ltd.','London','UK');
+
+-- Viết một câu lệnh INSERT INTO để thêm nhà cung cấp sau vào bảng Suppliers
+INSERT INTO Suppliers(
+	CompanyName,
+	ContactName,
+	ContactTitle,
+	Address,
+	City,
+	Region,
+	PostalCode,
+	Country,
+	Phone,
+	Fax,
+	HomePage)
+VALUES(
+	'Microsoft',
+	'John Smith',
+	'Sales Manager',
+	'123 Wall Street',
+	'New York',
+	'NY',
+	'10001',
+	'USA',
+	'555-555-5555',
+	'555-555-5556',
+	'http://www.newsupplier.com'
+);
+
+SELECT * FROM Suppliers;
+
+-- Hãy viết một câu lệnh INSERT INTO để thêm đơn hàng sau:
+-- CustomerID: chọn một mã khách hàng hiện có trong bảng Customers
+-- EmployeeID: chọn một mã nhân viên hiện có trong bảng Employees
+-- OrderDate: Sử dụng ngày hiện tại
+-- ShipVia: chọn một mã Shipper hiện có trong bảng Shippers
+INSERT INTO Orders(CustomerID,EmployeeID,OrderDate,ShipVia)
+VALUES('TITV',3,GETDATE(),3);
---
+++ MSSQL/bai_39/bai_39.sql
@@ -0,0 +1,70 @@
+﻿-- SELECT INTO trong SQL
+--* Cú pháp
+--SELECT *
+--INTO newtable [IN externaldb]
+--FROM oldtable
+--WHERE condition;
+
+--SELECT column1, column2, column3, ...
+--INTO newtable [IN externaldb]
+--FROM oldtable
+--WHERE condition;
+
+-- Tạo bảng mới với các sản phẩm có giá > 50
+SELECT *
+INTO HighValueProducts
+FROM Products
+WHERE UnitPrice > 50;
+
+SELECT * FROM HighValueProducts;
+
+-- tạo ra bảng mới với các đơn hàng được giao đến USA
+SELECT *
+INTO USAOrders
+FROM Orders
+WHERE ShipCountry = 'USA';
+
+SELECT * FROM USAOrders;
+
+-- Tạo một bảng thời "CustomersInLondon" từ bảng "Customers" trong
+-- CSDL NorthWind để chứa thông tin của các khách hàng có địa chỉ 
+-- ở London
+SELECT *
+INTO CustomersInLondon
+FROM Customers
+WHERE City = 'London';
+
+SELECT * FROM CustomersInLondon;
+
+-- tạo một bảng "HighValueOrders" để chứa thông tin về các
+-- đơn hàng có tổng giá trị đặt hàng lớn hơn 1000 đô la
+WITH ValueOver1000 AS(
+	SELECT od.OrderID 
+	FROM [Order Details] AS od
+	GROUP BY od.OrderID
+	HAVING SUM(od.UnitPrice * od.Quantity - od.UnitPrice * od.Quantity * od.Discount) > 1000
+)
+SELECT 
+	o.OrderID,
+	o.CustomerID,
+	o.EmployeeID,
+	o.OrderDate,
+	o.RequiredDate,
+	o.ShippedDate,
+	o.ShipVia,
+	o.Freight,
+	o.ShipName,
+	o.ShipAddress,
+	o.ShipCity,
+	o.ShipRegion,
+	o.ShipPostalCode,
+	o.ShipCountry
+INTO HighValueOrders
+FROM Orders AS o
+INNER JOIN ValueOver1000
+ON o.OrderID = ValueOver1000.OrderID;
+
+SELECT * FROM HighValueOrders;
+
+
+
