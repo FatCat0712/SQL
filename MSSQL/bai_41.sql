502d66166fdb37eaf674cc3b1a69b15d1575bd03
FatCat0712 <maisonhai456@gmail.com>
2024-03-07 09:57:19 +0700

    add index_database

---
+++ MSSQL/bai_41.sql
@@ -0,0 +1,38 @@
+﻿-- Update cập nhật dữ liệu trong bảng
+-- không cập nhật được PK
+-- Cú pháp
+-- UPDATE table_name
+-- SET column1 = value1, column2 = value2, ...
+-- WHERE condition
+-- không có mệnh đề WHERE thì toàn bộ dữ liệu của
+-- bảng sẽ được cập nhật
+
+SELECT * FROM Customers_1;
+
+-- cập nhật địa chỉ của khách hàng có mã là KH123
+UPDATE Customers_1
+SET ContactTitle = 'Finance Manager',Address = '51 Fremont Court Mcminnville'
+WHERE CustomerID = 'KH123';
+
+-- tăng toàn bộ giá của bảng Product lên 10% <=> UnitPrice * ((100 + 10)/100)
+
+UPDATE Products
+SET UnitPrice = UnitPrice * 1.1; 
+
+-- cập nhật thêm thông tin cho sản phẩm Mi hao hao
+UPDATE Products
+SET SupplierID = 3, 
+	CategoryID = 9,
+	QuantityPerUnit = '10 boxes',
+	UnitPrice = 15.50,
+	UnitsInStock = 36,
+	UnitsOnOrder = 20,
+	ReorderLevel = 10
+WHERE ProductName = 'Mi hao hao'
+
+SELECT * FROM Products;
+
+SELECT * FROM Customers_1;
+
+
+
---
+++ MSSQL/bai_42/bai_42.sql
@@ -0,0 +1,66 @@
+﻿SELECT * FROM INFORMATION_SCHEMA.TABLES;
+
+SELECT COUNT(*) 
+FROM Sales.SalesOrderDetail;
+
+-- Bật hiển thị thống kê tài nguyên I/O
+SET STATISTICS IO ON;
+
+-- câu truy vấn
+SELECT *
+FROM Sales.SalesOrderDetail
+WHERE CarrierTrackingNumber = '1B2B-492F-A9';
+
+-- Tắt thống kê I/O
+SET STATISTICS IO OFF;
+
+-- logical reads : số lần so sánh để lấy thông tin
+-- logical reads trước khi đánh index: 1238
+-- Tạo Index cho CarrierTrackingNumber
+CREATE INDEX idx_CarrierTrackingNumber
+ON Sales.SalesOrderDetail (CarrierTrackingNumber);
+
+-- logical reads sau khi đánh index: 69
+-- tốc độ đã tăng 1238 / 69 : 17 lần
+
+
+--Exercise
+-- Tạo index trên bảng Person.Address, cột AddressLine1 và 
+-- đánh giá hiệu suất truy vấn
+-- 1439 Brock Lane
+
+SET STATISTICS IO ON;
+SELECT *
+FROM Person.Address
+WHERE AddressLine1 = '1439 Brock Lane';
+SET STATISTICS IO OFF;
+
+-- logical reads : 5
+
+-- tạo index cho cột AddressLine1
+CREATE INDEX idx_AddressLine1
+ON Person.Address (AddressLine1);
+
+--drop index
+DROP INDEX idx_AddressLine1 ON Person.Address;
+
+
+
+SET STATISTICS IO ON;
+SELECT *
+FROM Person.Address
+WHERE AddressLine1 = '48995 Evergreen Wy.';
+SET STATISTICS IO OFF;
+-- logical reads: 5 => không thay đổi nhiều
+
+-- tạo index cho cột "ProductName" trong bảng "Production.Product"
+-- và đánh giá hiệu suất truy vấn
+
+-- LL Crankset
+SET STATISTICS IO ON;
+SELECT * 
+FROM Production.Product
+WHERE Name = 'LL Crankset';
+SET STATISTICS IO OFF;
+
+
